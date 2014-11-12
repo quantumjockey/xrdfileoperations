a8246dfc9012a32c86f15ba809aaea0bc32b62c9
Nicola DiPalma <nickyd242@gmail.com>
2014-11-11 18:29:25 -0800

    Separated variable initialization in image math classes.

--- src/xrdtiffoperations/math/DataSubtraction.java
+++ src/xrdtiffoperations/math/DataSubtraction.java
@@ -12,14 +12,15 @@ public class DataSubtraction {
     /////////// Public Methods ////////////////////////////////////////////////////////////////
 
     public static MARTiffImage subtractImages(MARTiffImage firstImage, MARTiffImage secondImage){
+        String filename;
+        int height, width;
+        WritableMARTiffImage temp;
 
-        String filename = generateFilename(firstImage, secondImage, false);
-
-        WritableMARTiffImage temp = new WritableMARTiffImage(filename);
+        filename = generateFilename(firstImage, secondImage, false);
+        temp = new WritableMARTiffImage(filename);
         temp.setIfdListing(firstImage.getIfdListing());
         temp.setByteOrder(firstImage.getByteOrder());
         temp.setCalibration(firstImage.getCalibration());
-        int height, width;
 
         height = (firstImage.getHeight() < secondImage.getHeight()) ? firstImage.getHeight() : secondImage.getHeight();
         width  = (firstImage.getWidth() < secondImage.getWidth()) ? firstImage.getWidth() : secondImage.getWidth();
@@ -31,15 +32,17 @@ public class DataSubtraction {
                 temp.setIntensityMapCoordinate(y, x, subtractIntensity(firstImage.getIntensityMapValue(y, x), secondImage.getIntensityMapValue(y, x)));
             }
         }
+
         return temp;
     }
 
     /////////// Private Methods /////////////////////////////////////////////////////////////////
 
     private static String generateFilename(MARTiffImage firstFile, MARTiffImage secondFile, boolean longName){
-        String result;
-        String firstSegment = stripFilename(firstFile.getFilename());
-        String secondSegment = stripFilename(secondFile.getFilename());
+        String firstSegment, result, secondSegment;
+
+        firstSegment = stripFilename(firstFile.getFilename());
+        secondSegment = stripFilename(secondFile.getFilename());
         if (longName){
             result = firstSegment + "_minus_" + secondSegment + DEFAULT_EXTENSION;
         }
@@ -47,6 +50,7 @@ public class DataSubtraction {
             String[] suffixParts = secondSegment.split("_");
             result = firstSegment + "_bknd_" + suffixParts[suffixParts.length - 1] + DEFAULT_EXTENSION;
         }
+
         return result;
     }
 
