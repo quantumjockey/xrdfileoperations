8aaec54efb43d5043b6207156e2521f930d9b065
Nicola DiPalma <nickyd242@gmail.com>
2014-11-28 19:00:29 -0800

    Created skeletons for unit-tests.

---
+++ test/xrdtiffoperations/filehandling/bytewrappers/ShortWrapperTest.java
@@ -0,0 +1,47 @@
+package xrdtiffoperations.filehandling.bytewrappers;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ShortWrapperTest {
+
+    /////////// Fields //////////////////////////////////////////////////////////////////////
+
+
+
+    /////////// Setup/Teardown //////////////////////////////////////////////////////////////
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    /////////// Tests ///////////////////////////////////////////////////////////////////////
+
+    @Test
+    public void get_shortWithinBoundsConverted_returnInput(){
+
+    }
+
+    @Test
+    public void get_shortBeyondBoundsConverted_returnZero(){
+
+    }
+
+    @Test
+    public void get_InvalidByteArraySize_returnZero(){
+
+    }
+
+    @Test
+    public void get_ShortBeyondStorableLimit_returnZero(){
+
+    }
+}
