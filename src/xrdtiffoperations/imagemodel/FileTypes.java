6b3190e165eca396cc373176a62baa851139b976
quantumjockey <nickyd242@gmail.com>
2014-12-06 23:35:23 -0800

    Created class for labeling file types.

---
+++ src/xrdtiffoperations/imagemodel/FileTypes.java
@@ -0,0 +1,12 @@
+package xrdtiffoperations.imagemodel;
+
+public class FileTypes {
+
+    /////////// Constants ///////////////////////////////////////////////////////////////////
+
+    public static final String TIFF_8_BIT_INT = "Tiff 8-Bit (Scaled)";
+    public static final String TIFF_16_BIT_INT = "Tiff 16-Bit (Scaled)";
+    public static final String TIFF_32_BIT_INT = "Tiff 32-Bit (Int)";
+    public static final String TIFF_32_BIT_FLOAT = "Tiff 32-Bit (Float)";
+
+}
