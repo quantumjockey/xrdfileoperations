59e542939718053700d2fa22fcc069da748f15fb
quantumjockey <nickyd242@gmail.com>
2015-04-16 18:22:35 -0700

    Created class for managing data manipulation tags/constants.

---
+++ src/main/java/edu/hipsec/xrdtiffoperations/datamapping/manipulation/GridTransform.java
@@ -0,0 +1,10 @@
+package edu.hipsec.xrdtiffoperations.datamapping.manipulation;
+
+public class GridTransform {
+
+    /////////// Constants ///////////////////////////////////////////////////////////////////
+
+    public static final String ROTATE_CLOCKWISE = "clockwise";
+    public static final String ROTATE_COUNTERCLOCKWISE = "counterclockwise";
+
+}
