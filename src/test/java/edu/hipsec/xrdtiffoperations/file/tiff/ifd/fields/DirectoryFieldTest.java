a0fb21edca2e0f227c48d973ab167842a0cd8350
Nicola DiPalma <nickyd242@gmail.com>
2015-04-22 13:06:57 -0700

    Created test class for DirectoryField.

--- src/test/java/edu/hipsec/xrdtiffoperations/file/tiff/ifd/fields/DirectoryFieldTest.java
+++ src/test/java/edu/hipsec/xrdtiffoperations/file/tiff/ifd/fields/DirectoryFieldTest.java
@@ -5,17 +5,19 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.nio.ByteOrder;
+
 public class DirectoryFieldTest {
 
     /////////// Fields //////////////////////////////////////////////////////////////////////
 
-
+    DirectoryField field;
 
     /////////// Setup/Teardown //////////////////////////////////////////////////////////////
 
     @Before
     public void setUp() throws Exception {
-
+        this.field = new DirectoryField();
     }
 
     @After
@@ -26,13 +28,22 @@ public class DirectoryFieldTest {
     /////////// Tests ///////////////////////////////////////////////////////////////////////
 
     @Test
-    public void constructor_checkArraySize_expectedByteLengthForType() {
-
+    public void fromByteArray_sourceArrayEmpty_conversionUnsuccessful() {
+        byte[] test = new byte[DirectoryField.BYTE_LENGTH];
+        Assert.assertFalse(this.field.fromByteArray(test, ByteOrder.BIG_ENDIAN));
     }
 
     @Test
-    public void get_characterConverted_returnInput() {
+    public void fromByteArray_sourceArraySmallerThanExpected_conversionUnsuccessful() {
+        byte[] test = new byte[3];
+        for (int i = 0; i < test.length; i++)
+            test[i] = (byte)i;
+        Assert.assertFalse(this.field.fromByteArray(test, ByteOrder.BIG_ENDIAN));
+    }
 
+    @Test
+    public void toByteArray_arrayGenerated_arraySizeSumOfExpectedFieldTypes() {
+        Assert.assertEquals(12, this.field.toByteArray(ByteOrder.BIG_ENDIAN));
     }
 
 }
