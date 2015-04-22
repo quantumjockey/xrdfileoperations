f27a3977573f7b516028f43cc75921732dac2611
Nicola DiPalma <nickyd242@gmail.com>
2015-04-21 18:43:18 -0700

    Completed RoundingOperator test.

--- src/main/java/edu/hipsec/xrdtiffoperations/utilities/trigonometry/RoundingOperator.java
+++ src/main/java/edu/hipsec/xrdtiffoperations/utilities/trigonometry/RoundingOperator.java
@@ -2,17 +2,28 @@ package edu.hipsec.xrdtiffoperations.utilities.trigonometry;
 
 public class RoundingOperator {
 
+    /////////// Constants ///////////////////////////////////////////////////////////////////
+
+    static final double RIGHT_ANGLE_INTERVAL = 90.0;
+
     /////////// Public Methods //////////////////////////////////////////////////////////////
 
     // preserves simple rotation for square grids
     public static double roundToRightAngle(double input) {
-        double remainder = input % 90.0;
+        int numRightAngles = (int)(input / RIGHT_ANGLE_INTERVAL);
+        double remainder = input - (numRightAngles * RIGHT_ANGLE_INTERVAL);
         double rounded = input;
 
         if (remainder != 0.0) {
-            rounded = input - remainder;
-            if (remainder >= 45.0)
-                rounded += 90.0;
+            if (Math.abs(remainder) >= RIGHT_ANGLE_INTERVAL / 2.0) {
+                if (input > 0.0)
+                    rounded = input + (RIGHT_ANGLE_INTERVAL - remainder);
+                else
+                    rounded = input - (RIGHT_ANGLE_INTERVAL + remainder);
+            }
+            else {
+                rounded = numRightAngles * RIGHT_ANGLE_INTERVAL;
+            }
         }
 
         return rounded;
