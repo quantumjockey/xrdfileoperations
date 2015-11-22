45c083cdeb1dd0f740e10c9e215c7066c9e59b1b
quantumjockey <nickyd242@gmail.com>
2015-11-21 23:15:28 -0800

    (TEMP) Extracted sources from repository root.
    Allows SBT project to be generated within the scope of the repository
    and sources re-inserted later.

--- src/main/java/edu/hipsec/xrdtiffoperations/utilities/delegate/TwoIntArgumentDelegate.java
+++
@@ -1,7 +0,0 @@
-package edu.hipsec.xrdtiffoperations.utilities.delegate;
-
-// Induces me to wish C#-like delegate constructs existed in Java, but this will do for now
-
-public interface TwoIntArgumentDelegate {
-    void callMethod(int a, int b);
-}
